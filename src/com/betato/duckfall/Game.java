6a8be0dca9e53d8ebf7b63d0ac5e6a6908854d79
Betato <BattlestarGatticus@gmail.com>
2016-06-10 16:30:19 -0400

    Working GameWindow
    Added Main class and main method.
    Added Game class which extends GameWindow.

---
+++ src/com/betato/duckfall/Game.java
@@ -0,0 +1,35 @@
+package com.betato.duckfall;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import com.betato.gamedisplay.GameWindow;
+import com.betato.gamedisplay.KeyStates;
+import com.betato.gamedisplay.MouseStates;
+
+public class Game extends GameWindow {
+
+	public Game() {
+		init(60, 120, "Duck Thing", new Dimension(720, 540), false, false);
+	}
+
+	@Override
+	public void onInit() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onExit() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onUpdate(KeyStates keys, MouseStates mouse, boolean resized) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onRender(Graphics g) {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ src/com/betato/duckfall/Main.java
@@ -0,0 +1,9 @@
+package com.betato.duckfall;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Game();
+	}
+
+}
