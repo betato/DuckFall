0e6a7441e3ecf4af0fc8116f49718100a517c693
Kevin <dragn194@gmail.com>
2016-06-11 22:50:53 -0400

    Refactor game logic into DuckFallScene class

---
+++ src/com/betato/duckfall/DuckFallScene.java
@@ -0,0 +1,76 @@
+package com.betato.duckfall;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import com.betato.gamedisplay.KeyStates;
+import com.betato.gamedisplay.MouseStates;
+
+public class DuckFallScene extends Scene {
+	
+	Duck duck;
+	ArrayList<Bag> bags;
+
+	public DuckFallScene(Game game) {
+		super(game);
+		background = game.loader.getTexture("background.png");
+	}
+	
+	@Override
+	public void enter() {
+		startGame();
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(KeyStates keys, MouseStates mouse) {
+		generateBags();
+		duck.update(keys, mouse);
+		//we loop backwards here to handle removals
+		for (int i = bags.size() - 1; i >= 0; i--){ // from 9 to 0
+			Bag bag = bags.get(i);
+			//remove any out of bounds bags
+			if (bag.isOutOfBounds()) {
+				bags.remove(i);
+			}
+			//otherwise, update and check for collisions
+			else {
+				bag.update(keys, mouse);
+				if (bag.isCollidingWith(duck)) {
+					System.out.println("QUACK");
+				}
+			}
+		}	
+		
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.drawImage(background, -2, -2, null);
+		duck.draw(g);
+		for (Bag bag : bags) {
+			bag.draw(g);
+		}
+		
+	}
+	
+	private void startGame() {
+		duck = new Duck(game, 64, 64);
+		bags = new ArrayList<>();
+	}
+	
+	private void generateBags() {
+		if (game.rng.nextInt(40) == 0) {
+			int i = game.rng.nextInt(64) + 32;
+			bags.add(new Bag(game, i, i, game.rng.nextInt(game.screenWidth),
+					-31, game.randomDouble(-0.4, 0.4), game.randomDouble(0.6, 3)));
+		}
+	}
+
+
+}
--- src/com/betato/duckfall/Game.java
+++ src/com/betato/duckfall/Game.java
@@ -13,30 +13,29 @@ import com.betato.gamedisplay.MouseStates;
 
 public class Game extends GameWindow {
 
-	private Entity duck;
-	private ArrayList<Bag> bags = new ArrayList<Bag>();
 	Random rng = new Random();
 	public static final int WINDOW_WIDTH = 720;
 	public static final int WINDOW_HEIGHT = 540;
 	public int screenHeight;
 	public int screenWidth;
-	private BufferedImage background;
 	
 	TextureLoader loader;
 	HighScoreServer highScoreServer;
 	
+	DuckFallScene scene;
+	
 	public Game() {
 		loader = new TextureLoader();
 		highScoreServer = new HighScoreServer();
-		background = loader.getTexture("background.png");
+		scene = new DuckFallScene(this);
+		scene.enter();
 		init(60, 120, "Duck Thing", new Dimension(720, 540), false, false, true);
 	}
 
 	@Override
 	public void onInit() {
 		screenHeight = getContentSize().height;
-		screenWidth = getContentSize().width;
-		duck = new Duck(this, 64, 64);
+		screenWidth = getContentSize().width;	
 	}
 
 	@Override
@@ -46,34 +45,10 @@ public class Game extends GameWindow {
 
 	@Override
 	public void onUpdate(KeyStates keys, MouseStates mouse, boolean resized) {
-		generateBags();
-		duck.update(keys, mouse);
-		//we loop backwards here to handle removals
-		for (int i = bags.size() - 1; i >= 0; i--){ // from 9 to 0
-			Bag bag = bags.get(i);
-			//remove any out of bounds bags
-			if (bag.isOutOfBounds()) {
-				bags.remove(i);
-			}
-			//otherwise, update and check for collisions
-			else {
-				bag.update(keys, mouse);
-				if (bag.isCollidingWith(duck)) {
-					System.out.println("QUACK");
-				}
-			}
-		}	
+		scene.update(keys, mouse);
 	}
 
-	private void generateBags() {
-		if (rng.nextInt(40) == 0) {
-			int i = rng.nextInt(64) + 32;
-			bags.add(new Bag(this, i, i, rng.nextInt(screenWidth),
-					-31, randomDouble(-0.4, 0.4), randomDouble(0.6, 3)));
-		}
-	}
-
-	private double randomDouble(double min, double max) {
+	public double randomDouble(double min, double max) {
 		return min + (max - min) * rng.nextDouble();
 	}
 
@@ -82,14 +57,8 @@ public class Game extends GameWindow {
 		// Clear background
 		g.setColor(Color.white);
 		g.fillRect(0, 0, screenWidth, screenHeight);
-		// Draw the background image. The position has to be at negative two, or else, there's a white border for some reason
-		g.drawImage(background, -2, -2, null);
-		// Draw duck and bags
-		duck.draw(g);
+		scene.draw(g);
 		g.drawString("FPS: " + fps, 10, 20);
 		g.drawString("UPS: " + ups, 10, 35);
-		for (Entity bag : bags){
-			bag.draw(g);
-		}
 	}
 }
